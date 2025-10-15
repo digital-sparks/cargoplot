c34108240dc952314d7bb8e0f154fee9f0eb1e73
vagifmdv <vagifmdv@gmail.com>
2025-10-16 03:56:59 +0400

    Added a Resource Hub swiper

---
+++ src/resource-hub.js
@@ -0,0 +1,51 @@
+import { gsap } from 'gsap';
+import { Observer } from 'gsap/Observer';
+import { SplitText } from 'gsap/SplitText';
+gsap.registerPlugin(Observer, SplitText);
+
+import Swiper from 'swiper';
+import {
+  Autoplay,
+  Navigation,
+  Pagination,
+  Scrollbar,
+  Keyboard,
+  Mousewheel,
+  A11y,
+} from 'swiper/modules';
+
+window.Webflow ||= [];
+window.Webflow.push(() => {
+  console.log('hello');
+
+  const featuredCarousel = new Swiper('.swiper-card-link_wrapper', {
+    modules: [Autoplay, Navigation, Pagination, Scrollbar, Keyboard, Mousewheel, A11y],
+    wrapperClass: 'swiper-card-link_list',
+    slideClass: 'swiper-card-link_slide',
+    slidesPerView: 'auto',
+    speed: 400,
+    spaceBetween: 24,
+    a11y: true,
+    grabCursor: true,
+    autoplay: false,
+    keyboard: {
+      onlyInViewport: true,
+    },
+    mousewheel: { forceToAxis: true },
+    navigation: {
+      prevEl: '.swiper_button.is-prev',
+      nextEl: '.swiper_button.is-next',
+    },
+    scrollbar: {
+      el: '.swiper_scrollbar',
+      dragClass: 'swiper_scrollbar-drag',
+      draggable: true,
+    },
+    breakpoints: {},
+    on: {
+      beforeInit: function () {
+        this.wrapperEl.style.columnGap = 'unset';
+      },
+    },
+  });
+});
